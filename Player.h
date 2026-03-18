8b55852c0273c7624cc10233f10e8c61f4e9097d
Sapana <dhakalsapana28@gmail.com>
2026-03-18 11:28:52 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,7 +3,7 @@
 
 #include <string>
 
-// Player class declaration — defines what a Player is and can do
+
 class Player {
 private:
     std::string name;
@@ -12,25 +12,25 @@ private:
     int attackPower;
 
 public:
-    // Default constructor
+    
     Player();
 
-    // Overloaded constructor
+
     Player(std::string name, int health, int attackPower);
 
-    // Destructor
+  
     ~Player();
 
-    // Mutator
+ 
     void takeDamage(int damage);
 
-    // Accessors (getters)
+   
     std::string getName() const;
     int getHealth() const;
     int getMaxHealth() const;
     int getAttackPower() const;
 
-    // Display helper
+    
     void displayStatus() const;
 };
 
